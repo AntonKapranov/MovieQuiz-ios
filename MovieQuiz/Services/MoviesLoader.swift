013170e55ffa55b57b7e8ddc0d93d81117d2e856
got_sin <gr8toad@gmail.com>
2024-05-30 01:34:20 +0300

    Подготовка к рефакторингу

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -6,13 +6,21 @@ protocol MoviesLoading {
 }
 
 struct MoviesLoader: MoviesLoading {
-    private let networkClient = NetworkClient()
-    private var mostPopularMoviesUrl:URL {
+  // MARK: - NetworkClient
+  private let networkClient: NetworkRouting
+  
+  init(networkClient: NetworkRouting = NetworkClient()) {
+      self.networkClient = networkClient
+  }
+    
+    // MARK: - URL
+    private var mostPopularMoviesUrl: URL {
         guard let url = URL(string: "https://tv-api.com/en/API/Top250Movies/k_zcuw1ytf") else {
-            preconditionFailure("Unable to construct")
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
         }
         return url
     }
+    
     func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
         networkClient.fetch(url: mostPopularMoviesUrl) { result in
             switch result {
@@ -28,6 +36,4 @@ struct MoviesLoader: MoviesLoading {
             }
         }
     }
-    
-    
 }
